06d5648f6f5b0a5b706a8cce227381f3d4add021
RDLSMODERNIZACION <modernizacion.mrdls@gmail.com>
2026-09-07 19:37:45 -0300

    Apply generic metrology labels on certificates page

--- front/app/certificados/page.tsx
+++ front/app/certificados/page.tsx
@@ -1,5 +1,11 @@
 import CertificatesView from "@/src/components/certificates/CertificatesView";
+import { GenericMetrologyLabels } from "@/src/components/certificates/GenericMetrologyLabels";
 
 export default function CertificadosPage() {
-  return <CertificatesView />;
+  return (
+    <>
+      <GenericMetrologyLabels />
+      <CertificatesView />
+    </>
+  );
 }
